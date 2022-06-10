9e1d92f349c2cc9f064d14d7fd71a8503fffd849
Evan Herman <evan.m.herman@gmail.com>
2022-06-10 18:06:31 -0400

    Update

---
+++ uninstall.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Handle cleaning up after the plugin on uninstall.
+ *
+ * @since 1.0.0
+ */
+
+// exit if uninstall constant is not defined
+if ( ! defined('WP_UNINSTALL_PLUGIN' ) ) {
+
+	exit;
+
+}
+
+delete_transient( 'lity_media' );
+delete_option( 'lity_options' );
