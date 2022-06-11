5fd18c1dcd879a7c5dbee687051a88e6d1b68254
Evan Herman <evan.m.herman@gmail.com>
2022-06-11 02:49:49 -0400

    Test

--- .dev/scripts/install-wp-tests.sh
+++ .dev/scripts/install-wp-tests.sh
@@ -11,8 +11,8 @@ DB_PASS=$3
 DB_HOST=${4-localhost}
 WP_VERSION=${5-latest}
 
-WP_TESTS_DIR=${WP_TESTS_DIR-/wp/wordpress-tests-lib}
-WP_CORE_DIR=${WP_CORE_DIR-/wp/wordpress/}
+WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
+WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
 
 download() {
     if [ `which curl` ]; then
@@ -22,17 +22,15 @@ download() {
     fi
 }
 
-mkdir wp
-
 if [[ $WP_VERSION =~ [0-9]+\.[0-9]+(\.[0-9]+)? ]]; then
 	WP_TESTS_TAG="tags/$WP_VERSION"
 elif [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
 	WP_TESTS_TAG="trunk"
 else
 	# http serves a single offer, whereas https serves multiple. we only want one
-	download http://api.wordpress.org/core/version-check/1.7/ /wp/wp-latest.json
-	grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /wp/wp-latest.json
-	LATEST_VERSION=$(grep -o '"version":"[^"]*' /wp/wp-latest.json | sed 's/"version":"//')
+	download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
+	grep '[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json
+	LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
 	if [[ -z "$LATEST_VERSION" ]]; then
 		echo "Latest WordPress version could not be found"
 		exit 1
@@ -51,18 +49,18 @@ install_wp() {
 	mkdir -p $WP_CORE_DIR
 
 	if [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
-		mkdir -p /wp/wordpress-nightly
-		download https://wordpress.org/nightly-builds/wordpress-latest.zip  /wp/wordpress-nightly/wordpress-nightly.zip
-		unzip -q /wp/wordpress-nightly/wordpress-nightly.zip -d /wp/wordpress-nightly/
-		mv /wp/wordpress-nightly/wordpress/* $WP_CORE_DIR
+		mkdir -p /tmp/wordpress-nightly
+		download https://wordpress.org/nightly-builds/wordpress-latest.zip  /tmp/wordpress-nightly/wordpress-nightly.zip
+		unzip -q /tmp/wordpress-nightly/wordpress-nightly.zip -d /tmp/wordpress-nightly/
+		mv /tmp/wordpress-nightly/wordpress/* $WP_CORE_DIR
 	else
 		if [ $WP_VERSION == 'latest' ]; then
 			local ARCHIVE_NAME='latest'
 		else
 			local ARCHIVE_NAME="wordpress-$WP_VERSION"
 		fi
-		download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /wp/wordpress.tar.gz
-		tar --strip-components=1 -zxmf /wp/wordpress.tar.gz -C $WP_CORE_DIR
+		download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
+		tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 	fi
 
 	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
--- .dev/tests/php/bootstrap.php
+++ .dev/tests/php/bootstrap.php
@@ -4,7 +4,7 @@ $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
 if ( ! $_tests_dir ) {
 
-	$_tests_dir = '/wp/wordpress-tests-lib';
+	$_tests_dir = '/tmp/wordpress-tests-lib';
 
 }
 
