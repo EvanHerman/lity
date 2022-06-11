b26e1ce72ddbe4c60a2a965760d68ff539d13ff7
Evan Herman <evan.m.herman@gmail.com>
2022-06-10 23:52:14 -0400

    jQuery no conflict

--- assets/js/lity-script.js
+++ assets/js/lity-script.js
@@ -1,120 +1,124 @@
-const lityMediaData = JSON.parse( lityScriptData.mediaData );
+( function( $ ) {
 
-/**
- * Lity Scripts
- *
- * @since 1.0.0
- */
-var lityScript = {
+	const lityMediaData = JSON.parse( lityScriptData.mediaData );
 
 	/**
-	 * Initialize the image with data-lity attributes
+	 * Lity Scripts
 	 *
 	 * @since 1.0.0
 	 */
-	init: function() {
+	const lityScript = {
 
-		jQuery( lityScriptData.imgSelectors ).not( lityScriptData.options.excluded_element_selectors ).attr( 'data-lity', '' );
+		/**
+		 * Initialize the image with data-lity attributes
+		 *
+		 * @since 1.0.0
+		 */
+		init: function() {
 
-	},
+			$( lityScriptData.imgSelectors ).not( lityScriptData.options.excluded_element_selectors ).attr( 'data-lity', '' );
 
-	/**
-	 * Add a data-lity-target attribute with the proper full size image URL.
-	 *
-	 * @since 1.0.0
-	 */
-	fullSizeImages: function() {
+		},
 
-		if ( 'no' === lityScriptData.options.show_full_size ) {
-			return;
-		}
+		/**
+		 * Add a data-lity-target attribute with the proper full size image URL.
+		 *
+		 * @since 1.0.0
+		 */
+		fullSizeImages: function() {
 
-		jQuery( lityScriptData.imgSelectors ).each( function( img ) {
-			if ( ! lityMediaData ) {
+			if ( 'no' === lityScriptData.options.show_full_size ) {
 				return;
 			}
 
-			let imgSrc = jQuery( this ).attr( 'src' );
-			let imgObj = [];
+			$( lityScriptData.imgSelectors ).each( function( img ) {
+				if ( ! lityMediaData ) {
+					return;
+				}
 
-			lityMediaData.forEach( ( media, index ) => {
-				if ( media.urls.includes( imgSrc ) ) {
-					imgObj.push( lityMediaData[ index ] );
+				let imgSrc = $( this ).attr( 'src' );
+				let imgObj = [];
+
+				lityMediaData.forEach( ( media, index ) => {
+					if ( media.urls.includes( imgSrc ) ) {
+						imgObj.push( lityMediaData[ index ] );
+					}
+				} );
+
+				if ( imgObj.length ) {
+					// make lity lightboxes show full sized versions of the image
+					$( this ).attr( 'data-lity-target', imgObj[0].urls[0] );
 				}
 			} );
 
-			if ( imgObj.length ) {
-				// make lity lightboxes show full sized versions of the image
-				jQuery( this ).attr( 'data-lity-target', imgObj[0].urls[0] );
+		},
+
+		/**
+		 * Append title and caption image info.
+		 *
+		 * @since 1.0.0
+		 */
+		appendImageInfo: function() {
+
+			if ( 'no' === lityScriptData.options.show_image_info ) {
+				return;
 			}
-		} );
 
-	},
+			$( lityScriptData.imgSelectors ).each( function( img ) {
+				let imgSrc = $( this ).attr( 'src' );
 
-	/**
-	 * Append title and caption image info.
-	 *
-	 * @since 1.0.0
-	 */
-	appendImageInfo: function() {
+				let imgObj = [];
 
-		if ( 'no' === lityScriptData.options.show_image_info ) {
-			return;
-		}
+				lityMediaData.forEach( ( media, index ) => {
+					if ( media.urls.includes( imgSrc ) ) {
+						imgObj.push( lityMediaData[ index ] );
+					}
+				} );
 
-		jQuery( lityScriptData.imgSelectors ).each( function( img ) {
-			let imgSrc = jQuery( this ).attr( 'src' );
+				if ( imgObj.length ) {
 
-			let imgObj = [];
+					if ( !! imgObj[0].title ) {
+						$( this ).attr( 'data-lity-title', imgObj[0].title );
+					}
 
-			lityMediaData.forEach( ( media, index ) => {
-				if ( media.urls.includes( imgSrc ) ) {
-					imgObj.push( lityMediaData[ index ] );
+					if ( !! imgObj[0].caption ) {
+						$( this ).attr( 'data-lity-description', imgObj[0].caption );
+					}
 				}
 			} );
 
-			if ( imgObj.length ) {
+		},
 
-				if ( !! imgObj[0].title ) {
-					jQuery( this ).attr( 'data-lity-title', imgObj[0].title );
-				}
+		/**
+		 * Show the image info in the lightbox when clicked.
+		 *
+		 * @since 1.0.0
+		 */
+		showImageInfo: function( event, lightbox ) {
 
-				if ( !! imgObj[0].caption ) {
-					jQuery( this ).attr( 'data-lity-description', imgObj[0].caption );
-				}
+			const triggerElement = lightbox.opener();
+			const title = triggerElement.data( 'lity-title' );
+			const description = triggerElement.data( 'lity-description' );
+
+			if ( !! title || !! description ) {
+				$( '.lity-content' ).addClass( 'lity-image-info' ).append( '<div class=lity-info></div>' );
 			}
-		} );
 
-	},
+			if ( !! title ) {
+				$( '.lity-info' ).append( '<h4>' + triggerElement.data( 'lity-title' ) + '</h4>' );
+			}
 
-	/**
-	 * Show the image info in the lightbox when clicked.
-	 *
-	 * @since 1.0.0
-	 */
-	showImageInfo: function( event, lightbox ) {
+			if ( !! description ) {
+				$( '.lity-info' ).append( '<p>' + triggerElement.data( 'lity-description' ) + '</p>' );
+			}
 
-		const triggerElement = lightbox.opener();
-		const title = triggerElement.data( 'lity-title' );
-		const description = triggerElement.data( 'lity-description' );
-
-		if ( !! title || !! description ) {
-			jQuery( '.lity-content' ).addClass( 'lity-image-info' ).append( '<div class=lity-info></div>' );
 		}
 
-		if ( !! title ) {
-			jQuery( '.lity-info' ).append( '<h4>' + triggerElement.data( 'lity-title' ) + '</h4>' );
-		}
+	};
 
-		if ( !! description ) {
-			jQuery( '.lity-info' ).append( '<p>' + triggerElement.data( 'lity-description' ) + '</p>' );
-		}
+	$( document ).on( 'ready', lityScript.init );
+	$( document ).on( 'ready', lityScript.fullSizeImages );
+	$( document ).on( 'ready', lityScript.appendImageInfo );
+	$( document ).on( 'lity:ready', lityScript.showImageInfo );
 
-	}
-
-};
-
-jQuery( document ).on( 'ready', lityScript.init );
-jQuery( document ).on( 'ready', lityScript.fullSizeImages );
-jQuery( document ).on( 'ready', lityScript.appendImageInfo );
-jQuery( document ).on( 'lity:ready', lityScript.showImageInfo );
+} )( jQuery );
