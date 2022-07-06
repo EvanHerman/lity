6f06726671c00661ce47f471688496c5294bb87a
Evan Herman <evan.m.herman@gmail.com>
2022-07-06 19:40:52 -0400

    rever package.json

--- .dev/tests/cypress/e2e/wp-5.4/lity-lightbox-tests.cy.js
+++ .dev/tests/cypress/e2e/wp-5.4/lity-lightbox-tests.cy.js
@@ -43,7 +43,7 @@ describe( 'Test Lity lightbox', () => {
 		} );
 
 		// Image size select field.
-		cy.get( '.components-panel__body .components-panel__body.is-opened:first-child select[id^=inspector-select-control-]' ).select( 'Full Size' );
+		cy.get( '.components-panel__body.is-opened:first-child select[id^=inspector-select-control-]' ).select( 'Full Size' );
 
 		cy.savePage54();
 
